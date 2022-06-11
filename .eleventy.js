382ad8c0d4f4dee995d7a5b48f52672cb28ba07a
Zuzana Volfova <zuzka.volfova@gmail.com>
2022-06-11 16:17:44 +0200

    add eleventy

---
+++ .eleventy.js
@@ -0,0 +1,26 @@
+module.exports = function(eleventyConfig) {
+
+	// Výchozí výstupní složka: _site
+
+	// Zkopírovat images/ do _site/images
+	eleventyConfig.addPassthroughCopy("images");
+
+	// Zkopírovat css/ to _site/css/
+	eleventyConfig.addPassthroughCopy("css");
+
+    // Zkopirovat download do _site/dowload
+    eleventyConfig.addPassthroughCopy("download");
+
+    eleventyConfig.addPassthroughCopy("javaScript");
+
+  return {
+    // možné formáty šablon
+    templateFormats: ["njk", "html", "md", "liquid"],
+
+    // jako šablonovací jazyk zvolíme Nunjucks
+    markdownTemplateEngine: "njk",
+    htmlTemplateEngine: "njk",
+    dataTemplateEngine: "njk",
+  }
+
+};
